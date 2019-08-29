a7d6f0250823c3d66235c4b14eab9e9aa6d294b0
Domenico Simone <dome.simone@gmail.com>
2019-08-29 08:55:30 +0200

    Symlinking sample libraries (see long descr)
    Each samples can have several libraries, and each library can have a different naming pattern. In order to simplify keyword handling by the snakemake workflow, we symlink each read file following a new scheme in the data/datasets.tab file, which now has a newly added column: "library". It works like this:
    
    sample          library R1                                      R2
    5517_hypo       1       5517_hypo_ATCACG_L003_R1_001.fastq.gz   5517_hypo_ATCACG_L003_R2_001.fastq.gz
    5517_hypo       2       5517_hypo_ATCACG_L004_R1_001.fastq.gz   5517_hypo_ATCACG_L004_R2_001.fastq.gz
    
    and 5517_hypo_ATCACG_L003_R1_001.fastq.gz will be symlinked as 5517_hypo_1.R1.fastq.gz.
    
    Edits:
    
    - snakefiles/variant_calling.snakefile
        - new rule symlink_libraries;
        - keyword {dataset_basename} replaced with {sample}_{library}
        - fixed definition of source_dir (broken due to 795b8f0)
    
    - modules/config_parsers.py
        - new functions: get_datasets_for_symlinks, new function get_symlinks
        - changed functions: get_sample_bamfiles, fastqc_raw_outputs, fastqc_filtered_outputs
    
    - cluster.yaml
        - fixed definitions for rules with wildcards.lane --> wildcards.library

--- modules/config_parsers.py
+++ modules/config_parsers.py
@@ -3,6 +3,24 @@ import pandas as pd
 import os
 from snakemake.io import *
 
+def get_datasets_for_symlinks(df, sample = None, library = None, d = None, infolder="data/reads", outfolder="data/reads"):
+    #print("sample = {sample}, library = {library}, d = {d}".format(sample = sample, library = library, d = d))
+    dataset_file = None
+    for row in df.itertuples():
+        if getattr(row, "sample") == sample and getattr(row, "library") == int(library):
+            dataset_file = os.path.join(outfolder, getattr(row, d))
+            #dataset_file = os.path.join(outfolder, "{sample}_{library}.{d}.fastq.gz".format(sample = sample, library = library, d = d))
+    #print(dataset_file)
+    return dataset_file
+
+def get_symlinks(df, analysis_tab = None, infolder="data/reads", outfolder="data/reads"):
+    outpaths = []
+    for i,l in df.iterrows():
+        if l["sample"] in list(analysis_tab["sample"]):
+            outpaths.append(os.path.join(outfolder, "{sample}_{library}.R1.fastq.gz".format(sample = l["sample"], library = l["library"])))
+            outpaths.append(os.path.join(outfolder, "{sample}_{library}.R2.fastq.gz".format(sample = l["sample"], library = l["library"])))
+    return outpaths
+
 def get_genome_single_vcf_files(df, res_dir="results", ref_genome_mt = None):
     outpaths = []
     for row in df.itertuples():
@@ -17,11 +35,30 @@ def get_genome_single_vcf_files(df, res_dir="results", ref_genome_mt = None):
             #                                                                                                             ref_genome_n = getattr(row, "ref_genome_n")))
     return outpaths
 
-def get_sample_bamfiles(df, res_dir="results", sample = None, ref_genome_mt = None, ref_genome_n = None):
+# def get_sample_bamfiles(df, res_dir="results", sample = None, ref_genome_mt = None, ref_genome_n = None):
+#     outpaths = []
+#     for row in df.itertuples():
+#         if getattr(row, "sample") == sample:
+#             bam_file = getattr(row, "R1").replace("_R1_001.fastq.gz", "_{ref_genome_mt}_{ref_genome_n}_OUT-sorted.final.bam".format(ref_genome_mt = ref_genome_mt, ref_genome_n = ref_genome_n))
+#             out_folder = "OUT_{base}".format(base = bam_file.replace("_OUT-sorted.final.bam", ""))
+#             outpaths.append("{results}/{sample}/map/{out_folder}/{bam_file}".format(results = res_dir, bam_file = bam_file, sample = sample, out_folder = out_folder))
+#             # outpaths.append("{results}/{sample}/OUT_{sample}_{lane}_{ref_genome_mt}_{ref_genome_n}/map/{sample}_{lane}_{ref_genome_mt}_{ref_genome_n}_OUT-sorted.bam".format(results = res_dir, \
+#             #                                                                                                                                                                                     sample = ))
+#             #
+#             # outpaths.append("{results}/OUT_{sample}_{ref_genome_mt}_{ref_genome_n}/{sample}_{ref_genome_mt}_{ref_genome_n}.vcf.gz".format(results = res_dir, \
+#             #                                                                                                             sample = getattr(row, "sample"), \
+#             #                                                                                                             ref_genome_mt = getattr(row, "ref_genome_mt"), \
+#             #                                                                                                             ref_genome_n = getattr(row, "ref_genome_n")))
+#     return outpaths
+
+#"{sample}_{library}.R1.fastq.gz".format(sample = l["sample"], library = l["library"])
+
+def get_sample_bamfiles(df, res_dir="results", sample = None, library = None, ref_genome_mt = None, ref_genome_n = None):
     outpaths = []
     for row in df.itertuples():
         if getattr(row, "sample") == sample:
-            bam_file = getattr(row, "R1").replace("_R1_001.fastq.gz", "_{ref_genome_mt}_{ref_genome_n}_OUT-sorted.final.bam".format(ref_genome_mt = ref_genome_mt, ref_genome_n = ref_genome_n))
+            #bam_file = 
+            bam_file = "{sample}_{library}_{ref_genome_mt}_{ref_genome_n}_OUT-sorted.final.bam".format(sample = sample, library = getattr(row, "library"), ref_genome_mt = ref_genome_mt, ref_genome_n = ref_genome_n)
             out_folder = "OUT_{base}".format(base = bam_file.replace("_OUT-sorted.final.bam", ""))
             outpaths.append("{results}/{sample}/map/{out_folder}/{bam_file}".format(results = res_dir, bam_file = bam_file, sample = sample, out_folder = out_folder))
             # outpaths.append("{results}/{sample}/OUT_{sample}_{lane}_{ref_genome_mt}_{ref_genome_n}/map/{sample}_{lane}_{ref_genome_mt}_{ref_genome_n}_OUT-sorted.bam".format(results = res_dir, \
@@ -100,19 +137,36 @@ def get_mt_genomes(df):
 def get_mt_fasta(df, ref_genome_mt, field):
     return df.loc[df['ref_genome_mt'] == ref_genome_mt, field][0]
 
+# def fastqc_raw_outputs(datasets_tab, analysis_tab = None, infolder="data/reads", outfolder="results/fastqc_raw", ext=".fastq.gz"):
+#     fastqc_out = []
+#     for i,l in datasets_tab.iterrows():
+#         if l["sample"] in list(analysis_tab["sample"]):
+#             fastqc_out.append(os.path.join(outfolder, l["R1"].replace(ext, "_fastqc.html")))
+#             fastqc_out.append(os.path.join(outfolder, l["R2"].replace(ext, "_fastqc.html")))
+#     return fastqc_out
+
 def fastqc_raw_outputs(datasets_tab, analysis_tab = None, infolder="data/reads", outfolder="results/fastqc_raw", ext=".fastq.gz"):
     fastqc_out = []
     for i,l in datasets_tab.iterrows():
         if l["sample"] in list(analysis_tab["sample"]):
-            fastqc_out.append(os.path.join(outfolder, l["R1"].replace(ext, "_fastqc.html")))
-            fastqc_out.append(os.path.join(outfolder, l["R2"].replace(ext, "_fastqc.html")))
+            fastqc_out.append(os.path.join(outfolder, "{sample}_{library}.R1_fastqc.html".format(sample = l["sample"], library = l["library"])))
+            fastqc_out.append(os.path.join(outfolder, "{sample}_{library}.R2_fastqc.html".format(sample = l["sample"], library = l["library"])))
     return fastqc_out
+
+# def fastqc_filtered_outputs(datasets_tab, analysis_tab = None, infolder="data/reads", outfolder="results/fastqc_filtered", ext="_001.fastq.gz"):
+#     fastqc_out = []
+#     for i,l in datasets_tab.iterrows():
+#         if l["sample"] in list(analysis_tab["sample"]):
+#             fastqc_out.append(os.path.join(outfolder, l["R1"].replace("_R1_001.fastq.gz", "_qc_R1_fastqc.html")))
+#             fastqc_out.append(os.path.join(outfolder, l["R2"].replace("_R2_001.fastq.gz", "_qc_R2_fastqc.html")))
+#             fastqc_out.append(os.path.join(outfolder, l["R1"].replace("_R1_001.fastq.gz", "_qc_U_fastqc.html")))
+#     return fastqc_out
 
 def fastqc_filtered_outputs(datasets_tab, analysis_tab = None, infolder="data/reads", outfolder="results/fastqc_filtered", ext="_001.fastq.gz"):
     fastqc_out = []
     for i,l in datasets_tab.iterrows():
         if l["sample"] in list(analysis_tab["sample"]):
-            fastqc_out.append(os.path.join(outfolder, l["R1"].replace("_R1_001.fastq.gz", "_qc_R1_fastqc.html")))
-            fastqc_out.append(os.path.join(outfolder, l["R2"].replace("_R2_001.fastq.gz", "_qc_R2_fastqc.html")))
-            fastqc_out.append(os.path.join(outfolder, l["R1"].replace("_R1_001.fastq.gz", "_qc_U_fastqc.html")))
+            fastqc_out.append(os.path.join(outfolder, "{sample}_{library}_qc_R1_fastqc.html".format(sample = l["sample"], library = l["library"])))
+            fastqc_out.append(os.path.join(outfolder, "{sample}_{library}_qc_R2_fastqc.html".format(sample = l["sample"], library = l["library"])))
+            fastqc_out.append(os.path.join(outfolder, "{sample}_{library}_qc_U_fastqc.html".format(sample = l["sample"], library = l["library"])))
     return fastqc_out
