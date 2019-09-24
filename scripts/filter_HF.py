af124db464c117cf530675306f0e730c00cdb67b
Domenico Simone <dome.simone@gmail.com>
2019-09-24 11:52:50 +0200

    filter_HF.py adapted to SDP and fixed bug with GT

--- scripts/filter_HF.py
+++ scripts/filter_HF.py
@@ -20,7 +20,7 @@ group_required.add_argument('--vcf-input', '-i', action="store", dest="vcf_input
 group_optional = parser.add_argument_group('optional arguments')
 group_optional.add_argument('--vcf-output', '-o', action="store", dest="vcf_output", help = "(default: <vcf_input>__filt_<hf_lower_threshold>_<hf_upper_threshold>.vcf)")
 group_optional.add_argument('--hf-lower-threshold', '-l', action="store", dest="hf_lower_threshold", type=float, default=0.20, help = "Must be <= 1 (default: %(default)s)")
-group_optional.add_argument('--hf-upper-threshold', '-u', action="store", dest="hf_upper_threshold", type=float, default=1, help = "default: %(default)s")
+group_optional.add_argument('--hf-upper-threshold', '-u', action="store", dest="hf_upper_threshold", type=float, default=1.0, help = "default: %(default)s")
 group_optional.add_argument("-h", "--help", action="help", help="show this help message and exit")
 
 args = parser.parse_args()
@@ -41,11 +41,16 @@ hf_upper_threshold = args.hf_upper_threshold
 
 if args.vcf_output == None:
     vcf_output = args.vcf_input.replace(".vcf", "__filt_{}_{}.vcf".format(hf_lower_threshold, hf_upper_threshold))
-    print("VCF output is {}".format(vcf_output))
+
+# Print some parameters
+print("VCF output is {}".format(vcf_output))
+print("HF lower threshold is {}".format(hf_lower_threshold))
+print("HF upper threshold is {}".format(hf_upper_threshold))
 
 VCF = vcf.Reader(filename = vcf_input)
 VCF_new = vcf.Writer(open(vcf_output, 'w'), VCF)
 
+# odict_keys(['GT', 'DP', 'HF', 'CILOW', 'CIUP', 'SDP'])
 f_keys = VCF.formats.keys()
 
 for vcf_record in VCF:
@@ -64,25 +69,35 @@ for vcf_record in VCF:
             GT_filtered = []
             CILOW_filtered = []
             CIUP_filtered = []
+            SDP_filtered = []
             if sum(to_list(data_dict['HF'])) < 1 - hf_lower_threshold:
                 GT_filtered.append(0)
             for x, i in enumerate(to_list(data_dict['HF'])):
                 if i >= hf_lower_threshold and i <= hf_upper_threshold:
                     HF_filtered.append(i)
-                    GT_filtered.append(GT[x+1]) # GT has REF too so the numbering is shifted
+                    # if the sample at this POS is heteroplasmic, len(GT) > 1 and '0' in GT.
+                    # GT has REF too so the numbering is shifted
+                    if len(GT) > 1:
+                        GT_filtered.append(GT[x+1])
+                    # else, grab the GT element with same index
+                    else:
+                        GT_filtered.append(GT[x]) # could be GT_filtered.append(GT[0]) as well
                     CILOW_filtered.append(to_list(data_dict['CILOW'])[x])
                     CIUP_filtered.append(to_list(data_dict['CIUP'])[x])
+                    SDP_filtered.append(to_list(data_dict['SDP'])[x])
         except: # fields might be empty
             HF_filtered = ["."]
             GT_filtered = [".", "."]
             CILOW_filtered = ["."]
             CIUP_filtered = ["."]
             DP = "."
+            SDP_filtered = ["."]
         new_vals = ['/'.join([str(n) for n in GT_filtered]), \
                    DP, \
                    HF_filtered, \
                    CILOW_filtered, \
-                   CIUP_filtered]
+                   CIUP_filtered, \
+                   SDP_filtered]
         vcf_record_new.samples[sx].data = vcf_record_new.samples[sx].data._make(new_vals)
     # write record only if at least one sample has something left
     ### prints left for testing
