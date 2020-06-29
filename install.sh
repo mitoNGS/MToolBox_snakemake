f2dea4254b8e9c7e16f781bea4dcfe8ac368495a
Domenico Simone <dome.simone@gmail.com>
2020-06-29 12:55:25 +0200

    install.sh fixed

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-conda env create -n mtoolbox -f envs/mtoolbox.yaml
+# conda env create -n mtoolbox -f envs/mtoolbox.yaml
 
 # create alias for env activation and running
-echo 'alias mtoolbox-activate="export PATH='`pwd`':'`pwd`'/scripts:$(conda run -n mtoolbox echo $PATH)"; conda activate mtoolbox' >> ~/.bash_profile
+echo 'alias mtoolbox-activate="export PATH='`pwd`':'`pwd`'/scripts:$(conda run -n mtoolbox echo $PATH); conda activate mtoolbox"' >> ~/.bash_profile
